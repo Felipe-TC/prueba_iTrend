bf9d5b05208132534f62e34279cf34621c7e22b1
Felipe Torres <felipe.torres.c@ug.uchile.cl>
2025-09-30 20:55:03 -0300

    Ejercicio de computo de ventas

--- estudio_iTrend/Ejercicio_csv.py
+++ estudio_iTrend/Ejercicio_csv.py
@@ -1,1 +1,18 @@
-aaaa
+# Importar librerias
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#print("Hola mundo")
+dv = pd.read_csv('datos_ventas.csv')
+
+#print(dv)
+dv_array = np.array(dv)
+
+#print(dv_array)
+#print(dv_array[0, 1])
+
+
+total_ventas_por_producto = dv_array[:, 2] * dv_array[:, 3]
+print(total_ventas_por_producto)
