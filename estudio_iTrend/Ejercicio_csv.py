4d7d2b2d3f5d8709819ab9665c3d25c57a4fb369
Felipe Torres <felipe.torres.c@ug.uchile.cl>
2025-09-30 21:37:23 -0300

    Modificacion de uso de numpy y arrays

--- estudio_iTrend/Ejercicio_csv.py
+++ estudio_iTrend/Ejercicio_csv.py
@@ -10,15 +10,15 @@ dv = pd.read_csv('datos_ventas.csv')
 #print(dv)
 dv_array = np.array(dv)
 
-print(dv_array)
+#print(dv_array)
 #print(dv_array[0, 1])
 
 
 total_ventas_por_producto = dv_array[:, 2] * dv_array[:, 3]
-print(total_ventas_por_producto)
+#print(total_ventas_por_producto)
 
 mean_of_sells = np.mean(dv_array[:, 2])
-print(mean_of_sells)
+#print(mean_of_sells)
 
 #Detectar si hay algun producto con venta total mayor a 1000
 
@@ -27,6 +27,11 @@ for valorsito in total_ventas_por_producto:
         print("efectivamente")
         break
 
-total_ventas_por_producto_con_nombres = np.vstack([np.transpose(dv_array[:, 1]), np.transpose(total_ventas_por_producto)])
-print(np.transpose(total_ventas_por_producto_con_nombres))
-print(total_ventas_por_producto_con_nombres[0])
+total_ventas_por_producto_con_nombres = np.vstack([dv_array[:, 1], total_ventas_por_producto])
+#print(np.transpose(total_ventas_por_producto_con_nombres))
+#print(total_ventas_por_producto_con_nombres[0])
+
+plt.bar(total_ventas_por_producto_con_nombres[0], total_ventas_por_producto_con_nombres[1])
+print(total_ventas_por_producto_con_nombres)
+print(total_ventas_por_producto_con_nombres[1])
+plt.show()
